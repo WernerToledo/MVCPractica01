daffb5736b15b7ab3d9edb90d7e6e6f6d0943c23
Alejandro Toledo <88223531+WernerToledo@users.noreply.github.com>
2023-03-31 21:52:51 -0600

    Agregar archivos de proyecto.

---
+++ MVCPractica01/Models/equiposContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace MVCPractica01.Models
+{
+    public class equiposContext : DbContext
+    {
+        public equiposContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<equipos> equipos {get; set;}
+    }
+}
