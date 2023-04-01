dc46012ed103fe1915aebc881f5c20e75f15a4a7
Alejandro Toledo <88223531+WernerToledo@users.noreply.github.com>
2023-03-31 22:43:41 -0600

    finish

--- MVCPractica01/Models/equiposContext.cs
+++ MVCPractica01/Models/equiposContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using wedApi_join.Model;
+
 namespace MVCPractica01.Models
 {
     public class equiposContext : DbContext
@@ -6,6 +8,15 @@ namespace MVCPractica01.Models
         public equiposContext(DbContextOptions options) : base(options)
         {
         }
-        public DbSet<equipos> equipos {get; set;}
+        public DbSet<equipos> equipos { get; set; }
+        public DbSet<estados_equipo> estados_equipo { get; set; }
+        public DbSet<marcas> marcas { get; set; }
+        public DbSet<tipo_equipo> tipo_equipo { get; set; }
+
+        public DbSet<estados_reserva> estados_reserva { get; set; }
+        public DbSet<facultades> facultades { get; set; }
+        public DbSet<reservas> reservas { get; set; }
+        public DbSet<usuarios> usuarios { get; set; }
+        public DbSet<carreras> carreras { get; set; }
     }
 }
